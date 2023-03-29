35d7ca89d42718355e734c4c3ea84ba8a432431d
Duc Tran <37317525+englishlayup@users.noreply.github.com>
2023-03-28 23:01:35 -0400

    feat: User can specify model with '-v'. Use 'gpt-4' by default

--- src/gpt.py
+++ src/gpt.py
@@ -5,14 +5,13 @@ from pathlib import Path
 
 openai.api_key_path = Path(__file__).parent.parent / ".env"
 
-
-def gpt(content, conversation=[]) -> str:
+def gpt(content, conversation=[], model="gpt-4") -> str:
     if not conversation:
         conversation = [
             {"role": "system", "content": "You are a helpful assistant."},
         ]
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model=model,
         messages=[
             *conversation,
             {"role": "user", "content": content},
@@ -22,13 +21,20 @@ def gpt(content, conversation=[]) -> str:
 
 
 def main(args: list[str] = sys.argv[1:]):
-    # TODO: format response nicely
+    #TODO: format response nicely
     # wrapped_response = textwrap.fill(
     #     gpt(args[0]),
     #     width=80,
     #     replace_whitespace=False,
     # )
-    sys.stdout.write(gpt(args[0]) + "\n")
+    model = 'gpt-4'
+    # set the model if '-v' is in args
+    if '-v' in args:
+        version = args[args.index('-v') + 1]
+        if version == '3':
+            model = 'gpt-3.5-turbo'
+
+    sys.stdout.write(gpt(args[-1], model=model) + "\n")
 
 
 if __name__ == "__main__":
